0d70038e028954193f672c373fdad02b08ac50d1
mpcdsx <mpcdsalazar@iskolarngbayan.pup.edu.ph>
2021-06-28 02:04:59 +0800

    DB

---
+++ Kanban.DL/SQL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace Kanban.DL
+{
+    public class SQL
+    {
+        static string connectionString = "Data Source=PAULASALAZAR;Initial Catalog=KanbanBoardApp;Integrated Security=True";
+        
+
+        static void ValidateUser() 
+        {
+            //Not done yet ..
+   
+        }
+    }
+}
